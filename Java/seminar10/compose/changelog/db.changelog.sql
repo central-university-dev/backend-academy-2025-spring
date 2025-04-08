4f40d7be90f0b8050cfd3a54d762a5bb9356a6f3
s.a.khvatov <s.a.khvatov@tinkoff.ru>
2025-04-08 18:14:38 +0300

    feat(seminar10): добавление docker-compose и настройка spring-batch-demo
    - Добавлен файл `docker-compose.yaml` для запуска PostgreSQL, Kafka, Zookeeper, Kafka UI и Schema Registry.
    - Обновлен `pom.xml` для управления зависимостями и добавления модуля `spring-batch-demo`.
    - Создан новый модуль `spring-batch-demo` с конфигурацией Spring Batch для обработки CSV данных.
    - Добавлены файлы конфигурации для Kafka UI и Liquibase.
    - Перемещен файл `application.yaml` в модуль `spring-batch-demo` и добавлены настройки для подключения к базе данных и источнику данных.
    - Добавлен файл `data.csv` с примером данных для обработки.
    - Добавлен файл `db.changelog.sql` для инициализации схемы базы данных.
    - Перемещен класс `SpringBatchApp` в модуль `spring-batch-demo`.

---
+++ Java/seminar10/compose/changelog/db.changelog.sql
@@ -0,0 +1,22 @@
+--liquibase formatted sql
+
+--changeset s.a.khvatov:1-init-schema
+create table accounts
+(
+    account_id     bigserial primary key,
+    account_number varchar(32) not null unique
+);
+
+create table account_scores
+(
+    account_id     bigserial primary key,
+    score          integer     not null,
+    updated_at     timestamp default now(),
+
+    constraint fk_accounts
+        foreign key (account_id) references accounts
+);
+
+
+
+
